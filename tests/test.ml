92a8e8a89fabb64f78b9dd5a964e1900713a046f
cedlemo <cedlemo@gmx.com>
2017-03-27 11:37:22 +0200

    Add tests

---
+++ tests/test.ml
@@ -0,0 +1,16 @@
+open OUnit2
+open GObjectIntrospection
+
+let test_load_namespace test_ctxt =
+  let namespace = "Gtk" in
+  let repo = GIRepository.get_default () in
+  let tplib = GIRepository.require repo namespace in
+  let c_prefix = GIRepository.get_c_prefix repo namespace in
+  assert_equal namespace c_prefix
+
+let gobject_introspection_tests =
+  "GObject Introspection tests" >:::
+    ["Load Gtk namespace" >:: test_load_namespace]
+
+let () =
+  run_test_tt_main gobject_introspection_tests
