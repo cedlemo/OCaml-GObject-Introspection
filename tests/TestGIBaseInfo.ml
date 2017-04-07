5bf070aaebce1c4654543ad0716c3e370c45a32e
cedlemo <cedlemo@gmx.com>
2017-04-07 15:58:19 +0200

    Add GIBaseInfo.get_namespace test

--- tests/TestGIBaseInfo.ml
+++ tests/TestGIBaseInfo.ml
@@ -23,10 +23,19 @@ let test_gibaseinfo_equal test_ctxt =
   | Some (info) -> let is_equal = GIBaseInfo.equal rand_info info in
   assert_equal_boolean true is_equal
 
+let test_gibaseinfo_get_namespace test_ctxt =
+  let namespace = "Gtk" in
+  let repo = load_namespace namespace in
+  let info_name = "init" in
+  match GIRepository.find_by_name repo namespace info_name with
+  | None -> assert_equal_string info_name "No base info found"
+  | Some (base_info) -> let info_namespace = GIBaseInfo.get_namespace base_info
+  in assert_equal_string namespace info_namespace
 
 let tests =
   "GObject Introspection BaseInfo tests" >:::
     [
      "GIBaseInfo get type" >:: test_gibaseinfo_get_type;
-     "GIBaseInfo equal" >:: test_gibaseinfo_equal
+     "GIBaseInfo equal" >:: test_gibaseinfo_equal;
+     "GIBaseInfo get namespace" >:: test_gibaseinfo_get_namespace
     ]
