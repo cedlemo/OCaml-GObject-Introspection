376717e0ebf062de15bfa70c099634a1157134df
cedlemo <cedlemo@gmx.com>
2017-04-09 18:36:21 +0200

    Add test for GIBaseInfo.get_container and TODO comments

--- tests/TestGIBaseInfo.ml
+++ tests/TestGIBaseInfo.ml
@@ -32,10 +32,22 @@ let test_gibaseinfo_get_namespace test_ctxt =
   | Some (base_info) -> let info_namespace = GIBaseInfo.get_namespace base_info
   in assert_equal_string namespace info_namespace
 
+(* TODO: fix error *)
+let test_gibaseinfo_get_container test_ctxt =
+  let namespace = "Gtk" in
+  let repo = load_namespace namespace in
+  let info_name = "Button" in
+  match GIRepository.find_by_name repo namespace info_name with
+  | None -> assert_equal_string info_name "No base info found"
+  | Some (base_info) -> let container = GIBaseInfo.get_container base_info
+  in let container_name = GIBaseInfo.get_name container in
+  assert_equal_string "nope" container_name
+
 let tests =
   "GObject Introspection BaseInfo tests" >:::
     [
      "GIBaseInfo get type" >:: test_gibaseinfo_get_type;
      "GIBaseInfo equal" >:: test_gibaseinfo_equal;
-     "GIBaseInfo get namespace" >:: test_gibaseinfo_get_namespace
+     "GIBaseInfo get namespace" >:: test_gibaseinfo_get_namespace(*;
+     "GIBaseInfo get container" >:: test_gibaseinfo_get_container *)
     ]
