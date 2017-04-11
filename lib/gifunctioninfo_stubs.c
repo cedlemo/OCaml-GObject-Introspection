4b9f14ceb927547505284faa03eedb67a61544ec
cedlemo <cedlemo@gmx.com>
2017-04-11 15:17:57 +0200

    Remove useless check

--- lib/gifunctioninfo_stubs.c
+++ lib/gifunctioninfo_stubs.c
@@ -22,9 +22,6 @@ static struct custom_operations gifunctioninfo_ops = {
 value
 alloc_gifunctioninfo (GIFunctionInfo *i)
 {
-    if(!GI_IS_FUNCTION_INFO (i))
-        caml_failwith ("The GIBaseInfo pointer is not a GIFunctionInfo");
-
     g_base_info_ref ((GIBaseInfo *)i);
     value v = alloc_custom(&gifunctioninfo_ops, sizeof (GIFunctionInfo *), 0, 1);
     GIFunctionInfo_val (v) = i;
