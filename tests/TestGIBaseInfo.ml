cc6edca1a5511180ba05739ff3be94981a68816c
cedlemo <cedlemo@gmx.com>
2017-04-11 11:59:38 +0200

    Simplify tests

--- tests/TestGIBaseInfo.ml
+++ tests/TestGIBaseInfo.ml
@@ -1,18 +1,18 @@
 open TestUtils
 open OUnit2
 
+let namespace = "Gio"
+let repo = GIRepository.get_default ()
+let typelib = GIRepository.require repo namespace
+let info_name = "app_info_create_from_commandline"
+
 let test_gibaseinfo_get_type test_ctxt =
-  let namespace = "Gtk" in
-  let repo = load_namespace namespace in
-  let info_name = "init" in
   match GIRepository.find_by_name (Some repo) namespace info_name with
   | None -> assert_equal_string info_name "No base info found"
   | Some (base_info) -> let info_type = GIBaseInfo.get_type base_info
   in assert_equal_string "function"  (GIBaseInfo.baseinfo_type_get_name info_type)
 
 let test_gibaseinfo_equal test_ctxt =
-  let namespace = "Gtk" in
-  let repo = load_namespace namespace in
   match GIRepository.get_info repo namespace 10 with
   | None -> assert_equal true false
   | Some (rand_info) -> let info_name = GIBaseInfo.get_name rand_info in
@@ -22,9 +22,6 @@ let test_gibaseinfo_equal test_ctxt =
   assert_equal_boolean true is_equal
 
 let test_gibaseinfo_get_namespace test_ctxt =
-  let namespace = "Gtk" in
-  let repo = load_namespace namespace in
-  let info_name = "init" in
   match GIRepository.find_by_name (Some repo) namespace info_name with
   | None -> assert_equal_string info_name "No base info found"
   | Some (base_info) -> let info_namespace = GIBaseInfo.get_namespace base_info
@@ -32,8 +29,6 @@ let test_gibaseinfo_get_namespace test_ctxt =
 
 (* TODO: fix error *)
 let test_gibaseinfo_get_container test_ctxt =
-  let namespace = "Gtk" in
-  let repo = load_namespace namespace in
   let info_name = "Button" in
   match GIRepository.find_by_name (Some repo) namespace info_name with
   | None -> assert_equal_string info_name "No base info found"
