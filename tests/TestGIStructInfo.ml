157c8f58ae21b974ea29a4428ce848a5b6054c80
cedlemo <cedlemo@gmx.com>
2017-04-13 15:04:37 +0200

    Add test for GIStructInfo.get_alignment

--- tests/TestGIStructInfo.ml
+++ tests/TestGIStructInfo.ml
@@ -14,6 +14,16 @@ let test_gistructinfo_from_baseinfo test_ctxt =
       | GIBaseInfo.Struct struct_info -> true
       | _ -> false)
 
+let test_gistructinfo_get_alignment test_ctxt =
+  match GIRepository.find_by_name (Some repo) namespace struct_name with
+  | None -> assert_equal_string struct_name "No base info found"
+  | Some (base_info) ->
+    match GIBaseInfo.get_type base_info with
+    | GIBaseInfo.Struct struct_info ->
+      let alignment = GIStructInfo.get_alignment struct_info in
+      assert_equal_int 2 alignment
+    | _ -> ()
+
 let tests =
   "GObject Introspection StructInfo tests" >:::
   [
