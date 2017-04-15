b9d7c33be53b8052c5fcc00055ee3a9d8d6ef54a
cedlemo <cedlemo@gmx.com>
2017-04-15 14:26:22 +0200

    Add license information

--- lib/conversions.c
+++ lib/conversions.c
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2017 Cedric LE MOIGNE, cedlemo@gmx.com
+ * This file is part of OCaml-GObject-Introspection.
+ *
+ * OCaml-GObject-Introspection is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * OCaml-GObject-Introspection is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OCaml-GObject-Introspection.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "conversions.h"
 
 value
