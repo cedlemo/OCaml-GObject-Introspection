a3b09d3a26c32d8713a84b548a8ff9a0ad4a7d49
cedlemo <cedlemo@gmx.com>
2017-04-24 12:07:37 +0200

    Adapt travis environment to ctypes bindings

--- .travis-build.sh
+++ .travis-build.sh
@@ -4,6 +4,8 @@ eval `opam config env`
 opam install ocamlfind
 opam install ounit
 opam install oasis
+opam install ctypes
+opam install ctypes-foreign
 sh .travis-gobject-introspection.sh
 ruby _oasis_conf.rb > _oasis
 sh build_and_test.sh
