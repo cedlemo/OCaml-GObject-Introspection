a4075fd28867c6ec004764d8214057d38b038f53
cedlemo <cedlemo@gmx.com>
2017-04-25 15:49:26 +0200

    Support GSList of strings to string list

--- lib/Conversions.ml
+++ lib/Conversions.ml
@@ -34,11 +34,29 @@ let glist_of_strings_to_list glist_ptr =
     | Some p' -> let data = g_list_data p' in
       let next = g_list_next p' in
       match coerce (ptr void) string_opt data with
-      | None -> loop acc next
+      | None -> loop ("toto" :: acc) next
       | Some s -> loop (s :: acc) next
   in loop [] glist_ptr
 
-(**
- * https://developer.gnome.org/glib/stable/glib-Doubly-Linked-Lists.html#GList
- * https://developer.gnome.org/glib/stable/glib-Doubly-Linked-Lists.html#g-list-next
- * https://github.com/ocamllabs/ocaml-ctypes/wiki/ctypes-tutorial#structs-and-unions *)
+type gslist
+let gslist : gslist structure typ = structure "GSList"
+let gslist_data  = field gslist "data" (ptr void)
+let gslist_next  = field gslist "next" (ptr_opt gslist)
+let () = seal gslist
+
+let g_slist_next l_ptr =
+  getf (!@l_ptr) gslist_next
+
+let g_slist_data l_ptr =
+  getf (!@l_ptr) gslist_data
+
+let gslist_of_strings_to_list gslist_ptr =
+  let rec loop acc p =
+    match p with
+    | None -> List.rev acc
+    | Some p' -> let data = g_slist_data p' in
+      let next = g_slist_next p' in
+      match coerce (ptr void) string_opt data with
+      | None -> loop acc next
+      | Some s -> loop (s :: acc) next
+  in loop [] gslist_ptr
