4f1b980f236d025fe5db0c8c162987d8cc3d686b
cedlemo <cedlemo@gmx.com>
2017-04-26 16:14:08 +0200

    Add GIFunctionInfo.get_symbol tests

--- tests/TestGIFunctionInfo.ml
+++ tests/TestGIFunctionInfo.ml
@@ -16,14 +16,24 @@
  * along with OCaml-GObject-Introspection.  If not, see <http://www.gnu.org/licenses/>.
  *)
 
+open TestUtils
 open OUnit2
 
-let () =
-  run_test_tt_main
-  ("GObjectIntrospection" >:::
-    [
-      TestGIRepository.tests;
-      TestGIBaseInfo.tests;
-      TestGIFunctionInfo.tests
-    ]
-  )
+let namespace = "GObject"
+let repo = GIRepository.get_default ()
+let typelib = GIRepository.require repo namespace
+
+let test_get_symbol test_ctxt =
+  let fn_name = "signal_name" in
+  match GIRepository.find_by_name repo namespace fn_name with
+  | None -> assert_equal_string fn_name "No base info found"
+  | Some (base_info) -> match GIBaseInfo.get_type base_info with
+    | GIBaseInfo.Function fn_info -> assert_equal "g_signal_name"
+                                                  (GIFunctionInfo.get_symbol fn_info)
+    | _ -> assert_equal_string "Base info" "Wrong info type"
+
+let tests =
+  "GObject Introspection FunctionInfo tests" >:::
+  [
+    "GIFunctionInfo get symbol" >:: test_get_symbol
+  ]
