6995001c62f04e8f86a563de7f66bc44abc681c3
cedlemo <cedlemo@gmx.com>
2017-04-27 18:22:38 +0200

    Add test for StructInfo in BaseInfo.get_type

--- tests/TestGIStructInfo.ml
+++ tests/TestGIStructInfo.ml
@@ -24,7 +24,17 @@ let repo = GIRepository.get_default ()
 let typelib = GIRepository.require repo namespace
 let struct_name = "Value"
 
+let test_baseinfo_get_type test_ctxt =
+  match GIRepository.find_by_name repo namespace struct_name with
+  | None -> assert_equal_string struct_name "No base info found"
+  | Some (base_info) -> assert_equal_boolean true (
+      match GIBaseInfo.get_type base_info with
+      | GIBaseInfo.Struct struct_info -> true
+      | _ -> false)
+
+
 let tests =
   "GObject Introspection StructInfo tests" >:::
   [
+    "GIStructInfo from BaseInfo" >:: test_baseinfo_get_type
   ]
