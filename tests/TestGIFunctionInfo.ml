5ac9b3afb8b0c816ab076195ec1ead40a68515c1
cedlemo <cedlemo@gmx.com>
2017-04-27 15:26:01 +0200

    Add GIFunctionInfo.get_flags test

--- tests/TestGIFunctionInfo.ml
+++ tests/TestGIFunctionInfo.ml
@@ -32,8 +32,18 @@ let test_get_symbol test_ctxt =
                                                   (GIFunctionInfo.get_symbol fn_info)
     | _ -> assert_equal_string "Base info" "Wrong info type"
 
+let test_get_flags test_ctxt =
+  let fn_name = "signal_name" in
+  match GIRepository.find_by_name repo namespace fn_name with
+  | None -> assert_equal_string fn_name "No base info found"
+  | Some (base_info) -> match GIBaseInfo.get_type base_info with
+    | GIBaseInfo.Function fn_info -> assert_equal []
+                                                  (GIFunctionInfo.get_flags fn_info)
+    | _ -> assert_equal_string "Base info" "Wrong info type"
+
 let tests =
   "GObject Introspection FunctionInfo tests" >:::
   [
-    "GIFunctionInfo get symbol" >:: test_get_symbol
+    "GIFunctionInfo get symbol" >:: test_get_symbol;
+    "GIFunctionInfo get flags" >:: test_get_flags
   ]
