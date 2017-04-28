fa3b4e8f3850c5a1fc0f695bb56eb909c2be9696
cedlemo <cedlemo@gmx.com>
2017-04-28 14:47:58 +0200

    Adapt GIFunctionInfo tests to new api

--- tests/TestGIFunctionInfo.ml
+++ tests/TestGIFunctionInfo.ml
@@ -28,8 +28,9 @@ let test_get_symbol test_ctxt =
   match GIRepository.find_by_name repo namespace fn_name with
   | None -> assert_equal_string fn_name "No base info found"
   | Some (base_info) -> match GIBaseInfo.get_type base_info with
-    | GIBaseInfo.Function fn_info -> assert_equal "g_signal_name"
-                                                  (GIFunctionInfo.get_symbol fn_info)
+    | GIBaseInfo.Function ->
+      let fn_info = GIFunctionInfo.functioninfo_of_baseinfo base_info in
+      assert_equal "g_signal_name" (GIFunctionInfo.get_symbol fn_info)
     | _ -> assert_equal_string "Base info" "Wrong info type"
 
 let test_get_flags test_ctxt =
@@ -37,8 +38,9 @@ let test_get_flags test_ctxt =
   match GIRepository.find_by_name repo namespace fn_name with
   | None -> assert_equal_string fn_name "No base info found"
   | Some (base_info) -> match GIBaseInfo.get_type base_info with
-    | GIBaseInfo.Function fn_info -> assert_equal []
-                                                  (GIFunctionInfo.get_flags fn_info)
+    | GIBaseInfo.Function ->
+      let fn_info = GIFunctionInfo.functioninfo_of_baseinfo base_info in
+      assert_equal [] (GIFunctionInfo.get_flags fn_info)
     | _ -> assert_equal_string "Base info" "Wrong info type"
 
 let tests =
