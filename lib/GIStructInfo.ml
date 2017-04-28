5bb230eb7dd122fa91cc38a590cb66a95f2e6d1b
cedlemo <cedlemo@gmx.com>
2017-04-28 15:18:27 +0200

    Add finalise instructions for GIFieldInfo created via get_field

--- lib/GIStructInfo.ml
+++ lib/GIStructInfo.ml
@@ -47,13 +47,20 @@ let get_n_methods =
   foreign "g_struct_info_get_n_methods"
     (ptr structinfo @-> returning int)
 
+let add_finaliser_to_field_info info =
+  let _ = Gc.finalise (fun i ->
+      let i' = GIFieldInfo.cast_fieldinfo_to_baseinfo i in
+      GIBaseInfo.base_info_unref i') info
+  in info
+
 let get_field info n =
   let get_field_raw =
     foreign "g_struct_info_get_field"
       (ptr structinfo @-> int @-> returning (ptr GIFieldInfo.fieldinfo)) in
   let max = get_n_fields info in
   if (n < 0 || n >= max) then raise (Failure "Array Index out of bounds")
-  else get_field_raw info n
+  else let info' = get_field_raw info n in
+    add_finaliser_to_field_info info'
 
 (* TODO : check that the info can be casted to a structinfo ? *)
 let cast_baseinfo_to_structinfo info =
