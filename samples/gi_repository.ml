a890f47177c0c4b4ced198c2a05488c152ab0e8a
cedlemo <cedlemo@gmx.com>
2017-05-05 16:15:53 +0200

    Add gi_repository sample

--- samples/gi_repository.ml
+++ samples/gi_repository.ml
@@ -22,26 +22,26 @@ let () =
   print_endline "Search path";
   let sp = GIRepository.get_search_path () in
   List.iter (fun x -> print_endline ("\t -" ^ x)) sp;
-  let _ = GIRepository.require (Some repo) namespace in
-  let c_prefix = GIRepository.get_c_prefix (Some repo) namespace in
-  let shared_lib = GIRepository.get_shared_library (Some repo) namespace in
-  let typelib_path = GIRepository.get_typelib_path (Some repo) namespace in
-  let version = GIRepository.get_version (Some repo) namespace in
+  let typelib = GIRepository.require repo namespace None 0 () in
+  let c_prefix = GIRepository.get_c_prefix repo namespace in
+(*  let shared_lib = GIRepository.get_shared_library repo namespace in *)
+  let typelib_path = GIRepository.get_typelib_path repo namespace in
+  let version = GIRepository.get_version repo namespace in
   print_endline ("Prefix:         " ^ c_prefix);
-  print_endline ("Shared library: " ^ shared_lib);
+  (* print_endline ("Shared library: " ^ shared_lib); *)
   print_endline ("Typelib path  : " ^ typelib_path);
   print_endline ("Version:        " ^ version);
   print_endline ("Versions:       ");
-  let versions = GIRepository.enumerate_versions (Some repo) namespace in
+  let versions = GIRepository.enumerate_versions repo namespace in
   List.iter (fun x -> print_endline ("\t v. : " ^ x)) versions;
   print_endline "Dependencies";
-  let dependencies = GIRepository.get_dependencies (Some repo) namespace in
+  let dependencies = GIRepository.get_dependencies repo namespace in
   List.iter (fun x -> print_endline ("\t dep. : " ^ x)) dependencies;
   print_endline "Immediate Dependencies";
 (*  let dependencies = GIRepository.get_immediate_dependencies (Some repo) namespace in
   List.iter (fun x -> print_endline ("\t dep. : " ^ x)) dependencies; *)
   print_endline "Loaded namespaces";
-  let namespaces = GIRepository.get_loaded_namespaces (Some repo) in
+  let namespaces = GIRepository.get_loaded_namespaces repo in
   List.iter (fun x -> print_endline ("\t namespace : " ^ x)) namespaces;
-  let n_infos = GIRepository.get_n_infos (Some repo) namespace in
+  let n_infos = GIRepository.get_n_infos repo namespace in
   print_endline ("Number of elements : " ^ (string_of_int n_infos))
