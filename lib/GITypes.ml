c47bb961d9c307e349cbdd7c90bcb404854166d9
cedlemo <cedlemo@gmx.com>
2017-05-05 15:57:11 +0200

    Add GTypes.argument

--- lib/GITypes.ml
+++ lib/GITypes.ml
@@ -116,3 +116,28 @@ let string_of_array_type = function
   | Ptr_array -> "ptr_array"
   | Byte_array -> "byte_array"
 
+type argument_t
+let argument : argument_t structure typ = structure "GIArgument"
+let v_boolean = field argument "v_boolean" (bool)
+let v_int8 = field argument "v_int8" (int8_t)
+let v_uint8 = field argument "v_uint8" (uint8_t)
+let v_int16 = field argument "v_int16" (int16_t)
+let v_uint16 = field argument "v_uint16" (uint16_t)
+let v_int32 = field argument "v_int32" (int32_t)
+let v_uint32 = field argument "v_uint32" (uint32_t)
+let v_int64 = field argument "v_int64" (int64_t)
+let v_uint64 = field argument "v_uint64" (uint64_t)
+let v_float = field argument "v_float" (float)
+let v_double = field argument "v_double" (double)
+let v_short = field argument "v_short" (short)
+let v_ushort = field argument "v_ushort" (ushort)
+let v_int = field argument "v_int" (int)
+let v_uint = field argument "v_uint" (uint)
+let v_long = field argument "v_long" (long)
+let v_ulong = field argument "v_ulong" (ulong)
+let v_ssize = field argument "v_ssize" (PosixTypes.ssize_t)
+let v_size = field argument "v_size" (size_t)
+let v_string = field argument "v_string" (string)
+let v_pointer = field argument "v_pointer" (ptr_opt void)
+
+let () = seal argument
