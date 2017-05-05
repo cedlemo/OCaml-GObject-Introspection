61c6beaf9fc28122af93ad1ea068ba207ed0903c
cedlemo <cedlemo@gmx.com>
2017-05-05 18:25:08 +0200

    Add GIConstant module

---
+++ lib/GIConstantInfo.mli
@@ -0,0 +1,28 @@
+(*
+ * Copyright 2017 Cedric LE MOIGNE, cedlemo@gmx.com
+ * This file is part of OCaml-GObject-Introspection.
+ *
+ * OCaml-GObject-Introspection is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * OCaml-GObject-Introspection is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OCaml-GObject-Introspection.  If not, see <http://www.gnu.org/licenses/>.
+ *)
+
+(** GIConstantInfo — Struct representing a constant *)
+
+open Ctypes
+
+(** GIConstantInfo represents a constant. A constant has a type associated
+    which can be obtained by calling GIConstantInfo.get_type and a value, which
+    can be obtained by calling GIConstant.get_value. *)
+type t
+val constantinfo : t structure typ
+
