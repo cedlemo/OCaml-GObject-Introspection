fedc308faf9d7916e2cd2a64aa34f6c70c2e5c9c
cedlemo <cedlemo@gmx.com>
2017-05-11 19:02:28 +0200

    Add GIObjectInfo.get_set_value_function test

--- tests/TestGIObjectInfo.ml
+++ tests/TestGIObjectInfo.ml
@@ -326,6 +326,13 @@ let test_gtk_window_get_unref_function test_ctxt =
       | Some _ -> assert_equal_string "It should not " "have any ref function"
     )
 
+let test_gtk_window_get_set_value_function test_ctxt =
+  object_test (fun info ->
+      match GIObjectInfo.get_set_value_function info with
+      | None -> assert_equal true true
+      | Some _ -> assert_equal_string "It should not " "have any set value function"
+    )
+
 let tests =
   "GObject Introspection ObjectInfo tests" >:::
   [
@@ -365,5 +372,6 @@ let tests =
     "GIObjectInfo GtkWindow get property" >:: test_gtk_window_get_property;
     "GIObjectInfo GtkWindow find method using interfaces" >:: test_gtk_window_find_method_using_interfaces;
     "GIObjectInfo GtkWindow get ref function" >:: test_gtk_window_get_ref_function;
-    "GIObjectInfo GtkWindow get unref function" >:: test_gtk_window_get_unref_function
+    "GIObjectInfo GtkWindow get unref function" >:: test_gtk_window_get_unref_function;
+    "GIObjectInfo GtkWindow get set value function" >:: test_gtk_window_get_set_value_function
   ]
