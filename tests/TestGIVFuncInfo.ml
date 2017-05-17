5348f903aa5f01d5f97fb1aeb9b43be33a725a25
cedlemo <cedlemo@gmx.com>
2017-05-17 18:49:51 +0200

    Add GIVFuncInfo test file

--- tests/TestGIVFuncInfo.ml
+++ tests/TestGIVFuncInfo.ml
@@ -16,27 +16,32 @@
  * along with OCaml-GObject-Introspection.  If not, see <http://www.gnu.org/licenses/>.
  *)
 
+open TestUtils
 open OUnit2
 
-let () =
-  run_test_tt_main
-  ("GObjectIntrospection" >:::
-    [
-      TestGIRepository.tests;
-      TestGIBaseInfo.tests;
-      TestGIFunctionInfo.tests;
-      TestGIStructInfo.tests;
-      TestGIUnionInfo.tests;
-      TestGIFieldInfo.tests;
-      TestGIEnumInfo.tests;
-      TestGICallableInfo.tests;
-      TestGIArgInfo.tests;
-      TestGITypeInfo.tests;
-      TestGIConstantInfo.tests;
-      TestGIObjectInfo.tests;
-      TestGIInterfaceInfo.tests;
-      TestGIPropertyInfo.tests;
-      TestGISignalInfo.tests;
-      TestGIVFuncInfo.tests
-    ]
-  )
+let namespace = "Gtk"
+let repo = GIRepository.get_default ()
+let typelib = GIRepository.require repo namespace None 0 ()
+let object_name = "Window"
+let vfunc_name = "activate_default"
+
+let get_vfunc_info () =
+  match GIRepository.find_by_name repo namespace object_name with
+  | None -> None
+  | Some (base_info) ->
+    match GIBaseInfo.get_type base_info with
+    | GIBaseInfo.Object -> let object_info = GIObjectInfo.from_baseinfo base_info in
+      GIObjectInfo.find_vfunc object_info vfunc_name
+    | _ -> None
+
+let vfunc_test fn =
+  match get_vfunc_info () with
+  | None -> assert_equal_string object_name "No base info found"
+  | Some (info) -> fn info
+
+let tests =
+  "GObject Introspection VFuncInfo tests" >:::
+  [
+  ]
+
+
