73461ebee5fd829a282e4a4fe9340640937c306e
cedlemo <cedlemo@gmx.com>
2017-05-24 21:06:54 +0200

    Improve loader_glib_viewer.ml

--- samples/loader_glib_viewer.ml
+++ samples/loader_glib_viewer.ml
@@ -7,7 +7,11 @@ let () =
   | Some _ -> let n = GIRepository.get_n_infos repo namespace in
     for i = 0 to n - 1 do
       let info = GIRepository.get_info repo namespace i in
-      match GIBaseInfo.get_name info with
-      | None -> print_endline "Anonymous"
-      | Some name -> print_endline name
+      let name = match GIBaseInfo.get_name info with
+        | None -> "Anonymous"
+        | Some name -> name
+      in let t = GIBaseInfo.get_type info in
+        let type_name = GIBaseInfo.string_of_baseinfo_type t in
+      let message = String.concat " : " [type_name; name] in
+        print_endline message
     done;
