7925ab5c0e9fe9aae104a24fc80104337723006b
cedlemo <cedlemo@gmx.com>
2017-05-26 19:54:18 +0200

    Add BuilderConstant test file

--- tests/TestBuilderConstant.ml
+++ tests/TestBuilderConstant.ml
@@ -16,30 +16,11 @@
  * along with OCaml-GObject-Introspection.  If not, see <http://www.gnu.org/licenses/>.
  *)
 
+open TestUtils
 open OUnit2
 
-let () =
-  run_test_tt_main
-  ("GObjectIntrospection" >:::
-    [
-      TestGIRepository.tests;
-      TestGIBaseInfo.tests;
-      TestGIFunctionInfo.tests;
-      TestGIStructInfo.tests;
-      TestGIUnionInfo.tests;
-      TestGIFieldInfo.tests;
-      TestGIEnumInfo.tests;
-      TestGICallableInfo.tests;
-      TestGIArgInfo.tests;
-      TestGITypeInfo.tests;
-      TestGIConstantInfo.tests;
-      TestGIObjectInfo.tests;
-      TestGIInterfaceInfo.tests;
-      TestGIPropertyInfo.tests;
-      TestGISignalInfo.tests;
-      TestGIVFuncInfo.tests;
-      TestGIRegisteredTypeInfo.tests;
-      TestLoader.tests;
-      TestBuilderConstant.tests
-    ]
-  )
+let tests =
+  "GObject Introspection BuilderConstant tests" >:::
+  [
+  ]
+
