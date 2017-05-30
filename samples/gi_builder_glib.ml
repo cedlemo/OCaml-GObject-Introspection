2c989069de9d5524c12c954575ac9d52660a75d8
cedlemo <cedlemo@gmx.com>
2017-05-30 18:54:31 +0200

    Add a sample for the Loader

---
+++ samples/gi_builder_glib.ml
@@ -0,0 +1,10 @@
+let print_infos loader =
+  let namespace = Loader.get_namespace loader in
+  let version = Loader.get_version loader in
+  print_endline (">> " ^ namespace);
+  print_endline ("\t - version :" ^ version)
+
+let () =
+  match Loader.load "GLib" () with
+  | None -> print_endline "Please check the namespace, something is wrong"
+  | Some loader -> print_infos loader; Loader.parse loader
