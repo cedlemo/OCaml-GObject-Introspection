f7ae4606701a306a23872e0a83602e0f789da0e0
cedlemo <cedlemo@gmx.com>
2017-06-05 15:15:15 +0200

    Start jbuilder support

---
+++ config/discover.ml
@@ -0,0 +1,27 @@
+open Base
+open Stdio
+module C = Configurator
+
+let write_sexp fn sexp =
+  Out_channel.write_all fn ~data:(Sexp.to_string sexp)
+
+let () =
+  C.main ~name:"GObject-Introspection" (fun c ->
+    let default : C.Pkg_config.package_conf =
+      { libs   = ["-lgirepository-1.0"; "-lgobject-2.0"; "-lglib-2.0"]
+      ; cflags = ["-O2"; "-Wall"; "-Wextra"; "-Wno-unused-parameter"; "-pthread";
+                  "-I/usr/include/gobject-introspection-1.0";
+                  "-I/usr/lib/libffi-3.2.1/include";
+                  "-I/usr/include/glib-2.0";
+                  "-I/usr/lib/glib-2.0/include"]
+      }
+    in
+    let conf =
+      match C.Pkg_config.get c with
+      | None -> default
+      | Some pc ->
+        Option.value (C.Pkg_config.query pc ~package:"gobject-introspection-1.0") ~default
+    in
+
+    write_sexp "c_flags.sexp"         (sexp_of_list sexp_of_string conf.libs);
+    write_sexp "c_library_flags.sexp" (sexp_of_list sexp_of_string conf.cflags))
