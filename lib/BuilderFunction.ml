dda50a9ed63d9f1216d9750a0d57b8381b9cff1f
cedlemo <cedlemo@gmx.com>
2017-07-05 18:38:50 +0200

    BuilderFunction.get_arguments_types transform no arguments to unit/void

--- lib/BuilderFunction.ml
+++ lib/BuilderFunction.ml
@@ -24,17 +24,18 @@ open BuilderUtils
  * the args for the ml file and the Ctypes functions binding *)
 let get_arguments_types callable =
   let n = GICallableInfo.get_n_args callable in
-  let rec parse_args index args_types =
-    if index = n then Some (List.rev args_types)
-    else let arg = GICallableInfo.get_arg callable index in
-      match GIArgInfo.get_direction arg with
-      | GIArgInfo.In -> let type_info = GIArgInfo.get_type arg in
-        let tag = GITypeInfo.get_tag type_info in
-        let (ocaml_type, ctypes_typ) = type_tag_to_ctypes_strings tag in
-        if ocaml_type = "" || ctypes_typ = "" then None
-        else parse_args (index + 1) ((ocaml_type, ctypes_typ) :: args_types)
-      | _ -> None
-  in parse_args 0 []
+  if n = 0 then Some [("unit", "void")]
+  else let rec parse_args index args_types =
+         if index = n then Some (List.rev args_types)
+         else let arg = GICallableInfo.get_arg callable index in
+           match GIArgInfo.get_direction arg with
+           | GIArgInfo.In -> let type_info = GIArgInfo.get_type arg in
+             let tag = GITypeInfo.get_tag type_info in
+             let (ocaml_type, ctypes_typ) = type_tag_to_ctypes_strings tag in
+             if ocaml_type = "" || ctypes_typ = "" then None
+             else parse_args (index + 1) ((ocaml_type, ctypes_typ) :: args_types)
+           | _ -> None
+    in parse_args 0 []
 
 let get_return_types callable =
   if GICallableInfo.skip_return callable then Some ("unit", "void")
