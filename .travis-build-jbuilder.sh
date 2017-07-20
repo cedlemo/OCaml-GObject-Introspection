d15bca00da4e178d7bb2b3aaa68815218559def6
cedlemo <cedlemo@gmx.com>
2017-07-20 19:28:24 +0200

    Try to use jbuilder to build and test in travis

---
+++ .travis-build-jbuilder.sh
@@ -0,0 +1,11 @@
+sh .travis-libffi.sh
+sh .travis-ocaml.sh
+export OPAMYES=1
+eval `opam config env`
+opam install ocamlfind
+opam install ounit
+opam install oasis
+opam install ctypes
+opam install ctypes-foreign
+sh .travis-gobject-introspection.sh
+jbuilder runtest
