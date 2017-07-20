2ff083f56943e06ce294a433dd25f23cd097d948
Rudi Grinberg <rudi.grinberg@gmail.com>
2017-07-20 00:56:48 -0400

    Add libffi to compiler flags

--- config/discover.ml
+++ config/discover.ml
@@ -20,7 +20,11 @@ let () =
       match C.Pkg_config.get c with
       | None -> default
       | Some pc ->
-        Option.value (C.Pkg_config.query pc ~package:"gobject-introspection-1.0") ~default
+        let libffi = Option.value_exn (C.Pkg_config.query pc ~package:"libffi") in
+        let gobject = Option.value (C.Pkg_config.query pc ~package:"gobject-introspection-1.0") ~default in
+        let  module P = C.Pkg_config in
+        { P.libs = libffi.P.libs @ gobject.P.libs
+        ; cflags = libffi.P.libs @ gobject.P.libs }
     in
 
     write_sexp "c_flags.sexp"         (sexp_of_list sexp_of_string conf.libs);
