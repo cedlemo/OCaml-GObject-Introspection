3dab557fb8958c0b4416ed5b306a43aa7c8de213
cedlemo <cedlemo@gmx.com>
2017-07-21 19:07:50 +0200

    Remove useless commented code

--- config/discover.ml
+++ config/discover.ml
@@ -29,12 +29,3 @@ let () =
 
     write_sexp "c_flags.sexp"         (sexp_of_list sexp_of_string conf.libs);
     write_sexp "c_library_flags.sexp" (sexp_of_list sexp_of_string conf.cflags))
-
-(*
- let libffi = Option.value_exn (C.Pkg_config.query pc ~package:"libffi") in
-         let gobject = Option.value (C.Pkg_config.query pc ~package:"gobject-introspection-1.0") ~default in
-         let  module P = C.Pkg_config in
-         { P.libs = libffi.P.libs @ gobject.P.libs
-         ; cflags = libffi.P.libs @ gobject.P.libs }
-     in
- * *)
