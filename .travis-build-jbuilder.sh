66145fbc9a6e5f292ed8e00db9495926ccb623b7
cedlemo <cedlemo@gmx.com>
2017-07-22 20:58:00 +0200

    Simplify travis configuration

--- .travis-build-jbuilder.sh
+++ .travis-build-jbuilder.sh
@@ -1,12 +1,3 @@
 sh .travis-libffi.sh
-sh .travis-ocaml.sh
-export OPAMYES=1
-eval `opam config env`
-opam install ocamlfind
-opam install ounit
-opam install oasis
-opam install ctypes
-opam install ctypes-foreign
-opam install jbuilder
 sh .travis-gobject-introspection.sh
-jbuilder runtest
+sh .travis-opam.sh
