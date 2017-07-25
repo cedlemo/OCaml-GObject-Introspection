7fcdc696574bc0dc9557138af65f378b748fe4bd
cedlemo <cedlemo@gmx.com>
2017-07-25 19:33:32 +0200

    Update to new api of GObjectIntrospection

--- samples/gi_builder_glib.ml
+++ samples/gi_builder_glib.ml
@@ -1,12 +1,14 @@
+module GI = GObjectIntrospection
+
 let print_infos loader =
-  let namespace = Loader.get_namespace loader in
-  let version = Loader.get_version loader in
+  let namespace = GI.Loader.get_namespace loader in
+  let version = GI.Loader.get_version loader in
   print_endline (">> " ^ namespace);
   print_endline ("\t - version :" ^ version)
 
 let () =
-  match Loader.load "GLib" () with
+  match GI.Loader.load "GLib" () with
   | None -> print_endline "Please check the namespace, something is wrong"
   | Some loader -> print_infos loader;
-    let loader = Loader.set_build_path loader "samples" in
-    Loader.parse loader ()
+    let loader = GI.Loader.set_build_path loader "samples" in
+    GI.Loader.parse loader ()
