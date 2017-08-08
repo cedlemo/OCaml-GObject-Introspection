91846efe9c3d1d53656db9cde781d29ffce917c5
cedlemo <cedlemo@gmx.com>
2017-08-08 18:34:45 +0200

    Another test for the flags

--- config/discover.ml
+++ config/discover.ml
@@ -31,7 +31,7 @@ let () =
          let libffi = Option.value (C.Pkg_config.query pc ~package:"libffi") ~default:default_ffi in
          let gobject = Option.value (C.Pkg_config.query pc ~package:"gobject-introspection-1.0") ~default in
          let  module P = C.Pkg_config in
-         { libs = libffi.P.libs @ gobject.P.libs;
+         { libs = (libffi.P.libs @ gobject.P.libs) @ ["-L/usr/lib/x86_64-linux-gnu";"-L/usr/lib"];
            cflags = (libffi.P.cflags @ gobject.P.cflags) @ ["-I/usr/include"; "-I/usr/include/x86_64-linux-gnu"] }
     in
     let _ = print_endline "==================== Configurator ================" in
