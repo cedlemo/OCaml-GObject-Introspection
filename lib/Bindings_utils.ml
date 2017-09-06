489722cc661e428a8a0f0681ee6667d1f93dc0f9
cedlemo <cedlemo@gmx.com>
2017-09-06 18:59:05 +0200

    Remove Bindings_utils.find_first_occurence remaining code

--- lib/Bindings_utils.ml
+++ lib/Bindings_utils.ml
@@ -164,32 +164,6 @@ let get_bindings_name info =
     let bindings_name = Lexer.snake_case name in
     Some bindings_name
 
-type pattern_found = NotFound | Position of int * int
-
-let find_first_occurence str pattern =
-  let str_len = String.length str in
-  let pattern_len = String.length pattern in
-  if str = pattern then Position (0, str_len)
-  else
-    if str_len < pattern_len then NotFound
-    else (
-      let check start =
-        let rec _check i =
-          let index = start + i in
-          if i = pattern_len then true
-          else
-            if str.[index] <> pattern.[i] then false
-            else _check (i + 1)
-        in _check 0
-      in
-      let rec _find j =
-        if j + pattern_len = str_len then NotFound
-        else
-          if check j then Position (j, pattern_len)
-          else _find (j + 1)
-        in _find 0
-      )
-
 let string_pattern_remove str pattern =
   let reg = Str.regexp_string pattern in
   String.concat "" (Str.split reg str)
