196eadd4f92ad29e9edd068167d645c57b3005fd
cedlemo <cedlemo@gmx.com>
2017-09-24 19:15:32 +0200

    Bind_struct: use new Binding_utils.File and Binding_utils.Sources api

--- lib/Bind_struct.ml
+++ lib/Bind_struct.ml
@@ -16,36 +16,39 @@
  * along with OCaml-GObject-Introspection.  If not, see <http://www.gnu.org/licenses/>.
  *)
 
-open Binding_utils
-
-let append_ctypes_struct_declaration name sources_files =
-  let (mli, ml) = sources_files in
-  Printf.fprintf mli "type t\n%!";
-  Printf.fprintf mli "val t_typ : t structure typ\n%!";
-  Printf.fprintf ml "type t\n%!";
-  Printf.fprintf ml "let t_typ : t structure typ = structure \"%s\"\n" name
+let append_ctypes_struct_declaration name sources =
+  let open Binding_utils in
+  let mli = Sources.mli sources in
+  let ml = Sources.ml sources in
+  File.bprintf mli "type t\n%!";
+  File.bprintf mli "val t_typ : t structure typ\n%!";
+  File.bprintf ml "type t\n%!";
+  File.bprintf ml "let t_typ : t structure typ = structure \"%s\"\n" name
 
 let handle_recursive_structure structure_name (ocaml_type, ctypes_typ) =
   if (structure_name ^ ".t structure") = ocaml_type then ("t structure", "t_typ")
   else if (structure_name ^ ".t structure ptr") = ocaml_type then ("t structure ptr", "ptr t_typ")
   else (ocaml_type, ctypes_typ)
 
-let append_ctypes_struct_fields_declarations struct_name info sources_files =
-  let (mli, ml) = sources_files in
+let append_ctypes_struct_fields_declarations struct_name info sources =
+  let open Binding_utils in
+  let mli = Sources.mli sources in
+  let ml = Sources.ml sources in
   let append_ctypes_struct_field_declarations field_info =
     let base_info = Field_info.to_baseinfo field_info in
     match Base_info.get_name base_info with
     | None -> ()
     | Some name ->
       let type_info = Field_info.get_type field_info in
-      match Binding_utils.type_info_to_bindings_types type_info false with
-      | Not_implemented tag_name -> let coms = Printf.sprintf "TODO Struct field %s : %s tag not implemented" struct_name tag_name in
-        Binding_utils.add_comments mli coms;
-        Binding_utils.add_comments ml coms
+      match type_info_to_bindings_types type_info false with
+      | Not_implemented tag_name ->
+        let coms = Printf.sprintf "TODO Struct field %s : %s tag not implemented" struct_name tag_name in
+        File.buff_add_comments mli coms;
+        File.buff_add_comments ml coms
       | Types {ocaml = ocaml_type; ctypes = ctypes_typ } ->
         let (ocaml_type', ctypes_typ') = handle_recursive_structure struct_name (ocaml_type, ctypes_typ) in
-        Printf.fprintf mli "val f_%s: (%s, t structure) field\n" name ocaml_type';
-        Printf.fprintf ml "let f_%s = field t_typ \"%s\" (%s)\n" name name ctypes_typ'
+        File.bprintf mli "val f_%s: (%s, t structure) field\n" name ocaml_type';
+        File.bprintf ml "let f_%s = field t_typ \"%s\" (%s)\n" name name ctypes_typ'
   in
   let n = Struct_info.get_n_fields info in
   for i = 0 to n - 1 do
@@ -53,8 +56,7 @@ let append_ctypes_struct_fields_declarations struct_name info sources_files =
     append_ctypes_struct_field_declarations field_info
   done
 
-(* hide while finishing Bind_function
- * let append_ctypes_struct_methods_bindings struct_name info sources_files =
+let append_ctypes_struct_methods_bindings struct_name info sources =
   let n = Struct_info.get_n_methods info in
   for i = 0 to n - 1 do
     let method_info = Struct_info.get_method info i in
@@ -62,22 +64,22 @@ let append_ctypes_struct_fields_declarations struct_name info sources_files =
     match Base_info.get_name base_info with
     | None -> ()
     | Some name ->
-        Bind_function.append_ctypes_method_bindings name method_info struct_name sources_files
+        Bind_function.append_ctypes_method_bindings name method_info struct_name sources
   done
-*)
-let append_ctypes_struct_seal ml_descr =
-  Printf.fprintf ml_descr "let _ = seal t_typ\n%!"
 
-let parse_struct_info info source_files =
+let append_ctypes_struct_seal file =
+  Binding_utils.File.bprintf file "let _ = seal t_typ\n%!"
+
+let parse_struct_info info sources =
+  let open Binding_utils in
   match get_binding_name info with
   | None -> ()
-  | Some name -> let f_descrs = (source_files.mli.descr,
-                                 source_files.ml.descr) in
-    let info' = Struct_info.from_baseinfo info in
-    append_ctypes_struct_declaration name f_descrs;
-    append_ctypes_struct_fields_declarations name info' f_descrs;
-    (* hide while finishing Bind_function append_ctypes_struct_methods_bindings name info' f_descrs; *)
-    add_empty_line source_files.mli.descr;
-    add_empty_line source_files.ml.descr
-
-
+  | Some name -> let info' = Struct_info.from_baseinfo info in
+    append_ctypes_struct_declaration name sources;
+    append_ctypes_struct_fields_declarations name info' sources;
+    append_ctypes_struct_methods_bindings name info' sources;
+    let mli = Sources.mli sources in
+    let ml = Sources.ml sources in
+    File.buff_add_eol mli;
+    File.buff_add_eol ml;
+    Sources.write_buffs sources
--- lib/Bind_struct.mli
+++ lib/Bind_struct.mli
@@ -23,16 +23,16 @@ open Ctypes
 open Foreign
 
 val append_ctypes_struct_declaration:
-  string -> (Pervasives.out_channel * Pervasives.out_channel) -> unit
+  string -> Binding_utils.Sources.t -> unit
 
 val append_ctypes_struct_fields_declarations:
-  string -> Struct_info.t structure ptr -> (Pervasives.out_channel * Pervasives.out_channel) -> unit
+  string -> Struct_info.t structure ptr -> Binding_utils.Sources.t -> unit
 
 (* val append_ctypes_struct_methods_bindings:
   string -> Struct_info.t structure ptr -> (Pervasives.out_channel * Pervasives.out_channel) -> unit
 *)
 val append_ctypes_struct_seal:
-  Pervasives.out_channel -> unit
+  Binding_utils.File.t -> unit
 
 (** Use GIStructureInfo in order to generate Ctypes bindings.
   - For each structure, a module is created in a StructureName.mli file and a StructureName.ml file.
@@ -41,4 +41,4 @@ val append_ctypes_struct_seal:
   - the fields are named `f_field_name` (in order to avoid conflict with OCaml keywords).
 *)
 val parse_struct_info :
-  Base_info.t structure ptr -> Binding_utils.files -> unit
+  Base_info.t structure ptr -> Binding_utils.Sources.t -> unit
