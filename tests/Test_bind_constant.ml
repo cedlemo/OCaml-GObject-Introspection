370423fa214b40e358902485772d0ee51dfee87b
cedlemo <cedlemo@gmx.com>
2017-09-26 19:25:18 +0200

    Adapt Bind_constant tests to Binding_utils.File and Sources api

--- tests/Test_bind_constant.ml
+++ tests/Test_bind_constant.ml
@@ -39,7 +39,14 @@ let constant_test namespace const_name fn =
 let test_append_boolean_constant test_ctxt =
   let namespace = "GLib" in
   let name = "SOURCE_REMOVE" in
-  let writer = Bind_constant.append_boolean_constant in
+  let writer = (fun info name sources ->
+    let open Binding_utils in
+    let mli = Sources.mli sources in
+    let ml = Sources.ml sources in
+    Bind_constant.append_boolean_constant info name (mli, ml);
+    Sources.write_buffs sources
+  )
+  in
   let mli_content = "val _SOURCE_REMOVE : bool" in
   let ml_content = "let _SOURCE_REMOVE = false" in
   constant_test namespace name (fun info ->
@@ -49,7 +56,14 @@ let test_append_boolean_constant test_ctxt =
 let test_append_int8_constant test_ctxt =
   let namespace = "GLib" in
   let name = "MAXINT8" in
-  let writer = Bind_constant.append_int8_constant in
+  let writer = (fun info name sources ->
+    let open Binding_utils in
+    let mli = Sources.mli sources in
+    let ml = Sources.ml sources in
+    Bind_constant.append_int8_constant info name (mli, ml);
+    Sources.write_buffs sources
+  )
+  in
   let mli_content = "val _MAXINT8 : int" in
   let ml_content = "let _MAXINT8 = 127" in
   constant_test namespace name (fun info ->
@@ -59,7 +73,14 @@ let test_append_int8_constant test_ctxt =
 let test_append_uint8_constant test_ctxt =
   let namespace = "GLib" in
   let name = "MAXUINT8" in
-  let writer = Bind_constant.append_uint8_constant in
+  let writer = (fun info name sources ->
+    let open Binding_utils in
+    let mli = Sources.mli sources in
+    let ml = Sources.ml sources in
+    Bind_constant.append_uint8_constant info name (mli, ml);
+    Sources.write_buffs sources
+  )
+  in
   let mli_content = "val _MAXUINT8 : Unsigned.uint8" in
   let ml_content = "let _MAXUINT8 = Unsigned.UInt8.of_int 255" in
   constant_test namespace name (fun info ->
@@ -69,7 +90,14 @@ let test_append_uint8_constant test_ctxt =
 let test_append_int16_constant test_ctxt =
   let namespace = "GLib" in
   let name = "MAXINT16" in
-  let writer = Bind_constant.append_int16_constant in
+  let writer = (fun info name sources ->
+    let open Binding_utils in
+    let mli = Sources.mli sources in
+    let ml = Sources.ml sources in
+    Bind_constant.append_int16_constant info name (mli, ml);
+    Sources.write_buffs sources
+  )
+  in
   let mli_content = "val _MAXINT16 : int" in
   let ml_content = "let _MAXINT16 = 32767" in
   constant_test namespace name (fun info ->
@@ -79,7 +107,14 @@ let test_append_int16_constant test_ctxt =
 let test_append_uint16_constant test_ctxt =
   let namespace = "GLib" in
   let name = "MAXUINT16" in
-  let writer = Bind_constant.append_uint16_constant in
+  let writer = (fun info name sources ->
+    let open Binding_utils in
+    let mli = Sources.mli sources in
+    let ml = Sources.ml sources in
+    Bind_constant.append_uint16_constant info name (mli, ml);
+    Sources.write_buffs sources
+  )
+  in
   let mli_content = "val _MAXUINT16 : Unsigned.uint16" in
   let ml_content = "let _MAXUINT16 = Unsigned.UInt16.of_int 65535" in
   constant_test namespace name (fun info ->
@@ -89,7 +124,14 @@ let test_append_uint16_constant test_ctxt =
 let test_append_int32_constant test_ctxt =
   let namespace = "GLib" in
   let name = "MAXINT32" in
-  let writer = Bind_constant.append_int32_constant in
+  let writer = fun info name sources -> (
+    let open Binding_utils in
+    let mli = Sources.mli sources in
+    let ml = Sources.ml sources in
+    Bind_constant.append_int32_constant info name (mli, ml);
+    Sources.write_buffs sources
+  )
+  in
   let mli_content = "val _MAXINT32 : int32" in
   let ml_content = "let _MAXINT32 = Int32.of_string \"2147483647\"" in
   constant_test namespace name (fun info ->
@@ -99,7 +141,14 @@ let test_append_int32_constant test_ctxt =
 let test_append_uint32_constant test_ctxt =
   let namespace = "GLib" in
   let name = "MAXUINT32" in
-  let writer = Bind_constant.append_uint32_constant in
+  let writer = (fun info name sources ->
+    let open Binding_utils in
+    let mli = Sources.mli sources in
+    let ml = Sources.ml sources in
+    Bind_constant.append_uint32_constant info name (mli, ml);
+    Sources.write_buffs sources
+  )
+  in
   let mli_content = "val _MAXUINT32 : Unsigned.uint32" in
   let ml_content = "let _MAXUINT32 = Unsigned.UInt32.of_string \"4294967295\"" in
   constant_test namespace name (fun info ->
@@ -109,7 +158,14 @@ let test_append_uint32_constant test_ctxt =
 let test_append_int64_constant test_ctxt =
   let namespace = "GLib" in
   let name = "MAXINT64" in
-  let writer = Bind_constant.append_int64_constant in
+  let writer = (fun info name sources ->
+    let open Binding_utils in
+    let mli = Sources.mli sources in
+    let ml = Sources.ml sources in
+    Bind_constant.append_int64_constant info name (mli, ml);
+    Sources.write_buffs sources
+  )
+  in
   let mli_content = "val _MAXINT64 : int64" in
   let ml_content = "let _MAXINT64 = 9223372036854775807L" in
   constant_test namespace name (fun info ->
@@ -119,7 +175,14 @@ let test_append_int64_constant test_ctxt =
 let test_append_uint64_constant test_ctxt =
   let namespace = "GLib" in
   let name = "MAXUINT64" in
-  let writer = Bind_constant.append_uint64_constant in
+  let writer = fun info name sources -> (
+    let open Binding_utils in
+    let mli = Sources.mli sources in
+    let ml = Sources.ml sources in
+    Bind_constant.append_uint64_constant info name (mli, ml);
+    Sources.write_buffs sources
+  )
+  in
   let mli_content = "val _MAXUINT64 : Unsigned.uint64" in
   let ml_content = "let _MAXUINT64 = Unsigned.UInt64.of_string \"18446744073709551615\"" in
   constant_test namespace name (fun info ->
@@ -131,17 +194,31 @@ let test_append_uint64_constant test_ctxt =
 let test_append_double_constant test_ctxt =
   let namespace = "GLib" in
   let name = "E" in
-  let writer = Bind_constant.append_double_constant in
+  let writer = (fun info name sources ->
+    let open Binding_utils in
+    let mli = Sources.mli sources in
+    let ml = Sources.ml sources in
+    Bind_constant.append_double_constant info name (mli, ml);
+    Sources.write_buffs sources
+  )
+  in
   let mli_content = "val _E : float" in
   let ml_content = "let _E = 2.718282" in
   constant_test namespace name (fun info ->
       test_writing test_ctxt info name writer mli_content ml_content
-    )
+  )
 
 let test_append_string_constant test_ctxt =
   let namespace = "GLib" in
   let name = "CSET_A_2_Z" in
-  let writer = Bind_constant.append_string_constant in
+  let writer = (fun info name sources ->
+    let open Binding_utils in
+    let mli = Sources.mli sources in
+    let ml = Sources.ml sources in
+    Bind_constant.append_string_constant info name (mli, ml);
+    Sources.write_buffs sources
+  )
+  in
   let mli_content = "val _CSET_A_2_Z : string" in
   let ml_content = "let _CSET_A_2_Z = \"ABCDEFGHIJKLMNOPQRSTUVWXYZ\"" in
   constant_test namespace name (fun info ->
