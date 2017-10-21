a14c42f0f98987941c3fbd2bcf6fca55298ca46f
cedlemo <cedlemo@gmx.com>
2017-10-21 19:07:44 +0200

    glib_raw_generator: use module alias to simplify

--- tools/glib_raw_generator.ml
+++ tools/glib_raw_generator.ml
@@ -1,4 +1,13 @@
 module GI = GObject_introspection
+module Base_info = GI.Base_info
+module Bind_constant = GI.Bind_constant
+module Bind_function = GI.Bind_function
+module Binding_utils = GI.Binding_utils
+module Constant_info = GI.Constant_info
+module Function_info = GI.Function_info
+module Loader = GI.Loader
+module Types = GI.Types
+module Type_info = GI.Type_info
 
 (* This tool is used to  generate the raw bindings of GLib 2 in
  * the OCaml-glib package
@@ -7,8 +16,8 @@ module GI = GObject_introspection
  *)
 
 let print_infos loader =
-  let namespace = GI.Loader.get_namespace loader in
-  let version = GI.Loader.get_version loader in
+  let namespace = Loader.get_namespace loader in
+  let version = Loader.get_version loader in
   print_endline (">> " ^ namespace);
   print_endline ("\t - version :" ^ version)
 
@@ -25,15 +34,15 @@ let skip = [ "Hook";
 ]
 
 let const_parser info sources =
-  let open GI.Binding_utils in
-  let open GI.Bind_constant in
-  match GI.Base_info.get_name info with
+  let open Binding_utils in
+  let open Bind_constant in
+  match Base_info.get_name info with
   | None -> ()
   | Some name ->
-    let info' = GI.Constant_info.from_baseinfo info in
-    let type_info = GI.Constant_info.get_type info' in
+    let info' = Constant_info.from_baseinfo info in
+    let type_info = Constant_info.get_type info' in
     let not_implemented_todo_comments tag (mli, ml) =
-      let tag_name = GI.Types.string_of_tag tag in
+      let tag_name = Types.string_of_tag tag in
       File.bprintf mli "(* TODO : constant %s type not implemented for %s *)" name tag_name;
       File.bprintf mli "(* TODO : constant %s type not implemented for %s *)" name tag_name
     in
@@ -48,36 +57,36 @@ let const_parser info sources =
     match name with
     | "MAJOR_VERSION" | "MINOR_VERSION" | "MICRO_VERSION" ->
        dyn_loader_version_number_constants ()
-    | _ -> let _ = match GI.Type_info.get_tag type_info with
-      | GI.Types.Void as tag -> not_implemented_todo_comments tag (mli, ml)
-      | GI.Types.Boolean -> append_boolean_constant name info' (mli, ml)
-      | GI.Types.Int8 -> append_int8_constant name info' (mli, ml)
-      | GI.Types.Uint8 -> append_uint8_constant name info' (mli, ml)
-      | GI.Types.Int16 -> append_int16_constant name info' (mli, ml)
-      | GI.Types.Uint16 -> append_uint16_constant name info' (mli, ml)
-      | GI.Types.Int32 -> append_int32_constant name info' (mli, ml)
-      | GI.Types.Uint32 -> append_uint32_constant name info' (mli, ml)
-      | GI.Types.Int64 -> append_int64_constant name info' (mli, ml)
-      | GI.Types.Uint64 -> append_uint64_constant name info' (mli, ml)
-      | GI.Types.Float -> append_float_constant name info' (mli, ml)
-      | GI.Types.Double -> append_double_constant name info' (mli, ml)
-      | GI.Types.GType as tag -> not_implemented_todo_comments tag (mli, ml)
-      | GI.Types.Utf8 -> append_string_constant name info' (mli, ml)
-      | GI.Types.Filename as tag -> not_implemented_todo_comments tag (mli, ml)
-      | GI.Types.Array as tag -> not_implemented_todo_comments tag (mli, ml)
-      | GI.Types.Interface as tag -> not_implemented_todo_comments tag (mli, ml)
-      | GI.Types.GList as tag -> not_implemented_todo_comments tag (mli, ml)
-      | GI.Types.GSList as tag -> not_implemented_todo_comments tag (mli, ml)
-      | GI.Types.GHash as tag -> not_implemented_todo_comments tag (mli, ml)
-      | GI.Types.Error as tag -> not_implemented_todo_comments tag (mli, ml)
-      | GI.Types.Unichar as tag -> not_implemented_todo_comments tag (mli, ml)
+    | _ -> let _ = match Type_info.get_tag type_info with
+      | Types.Void as tag -> not_implemented_todo_comments tag (mli, ml)
+      | Types.Boolean -> append_boolean_constant name info' (mli, ml)
+      | Types.Int8 -> append_int8_constant name info' (mli, ml)
+      | Types.Uint8 -> append_uint8_constant name info' (mli, ml)
+      | Types.Int16 -> append_int16_constant name info' (mli, ml)
+      | Types.Uint16 -> append_uint16_constant name info' (mli, ml)
+      | Types.Int32 -> append_int32_constant name info' (mli, ml)
+      | Types.Uint32 -> append_uint32_constant name info' (mli, ml)
+      | Types.Int64 -> append_int64_constant name info' (mli, ml)
+      | Types.Uint64 -> append_uint64_constant name info' (mli, ml)
+      | Types.Float -> append_float_constant name info' (mli, ml)
+      | Types.Double -> append_double_constant name info' (mli, ml)
+      | Types.GType as tag -> not_implemented_todo_comments tag (mli, ml)
+      | Types.Utf8 -> append_string_constant name info' (mli, ml)
+      | Types.Filename as tag -> not_implemented_todo_comments tag (mli, ml)
+      | Types.Array as tag -> not_implemented_todo_comments tag (mli, ml)
+      | Types.Interface as tag -> not_implemented_todo_comments tag (mli, ml)
+      | Types.GList as tag -> not_implemented_todo_comments tag (mli, ml)
+      | Types.GSList as tag -> not_implemented_todo_comments tag (mli, ml)
+      | Types.GHash as tag -> not_implemented_todo_comments tag (mli, ml)
+      | Types.Error as tag -> not_implemented_todo_comments tag (mli, ml)
+      | Types.Unichar as tag -> not_implemented_todo_comments tag (mli, ml)
     in
     Sources.write_buffs sources
 
 let function_parser info sources skip_types =
-  let open GI.Binding_utils in
-  let open GI.Bind_function in
-  match GI.Base_info.get_name info with
+  let open Binding_utils in
+  let open Bind_function in
+  match Base_info.get_name info with
   | None -> ()
   | Some name ->
       if name = "check_version" then (
@@ -90,23 +99,23 @@ let function_parser info sources skip_types =
 
       )
       else (
-        let _ = match GI.Base_info.get_container info with
+        let _ = match Base_info.get_container info with
         | None -> ()
-        | Some container -> match GI.Base_info.get_name container with
+        | Some container -> match Base_info.get_name container with
            | None -> ()
            | Some container_name -> print_endline (String.concat " " ["Container :";
                                                                       container_name;
                                                                       "function";
                                                                       name])
        in
-       let info' = GI.Function_info.from_baseinfo info in
+       let info' = Function_info.from_baseinfo info in
        append_ctypes_function_bindings name info' sources skip_types
       );
      Sources.write_buffs sources
 
 let () =
-  match GI.Loader.load "GLib" () with
+  match Loader.load "GLib" () with
   | None -> print_endline "Please check the namespace, something is wrong"
   | Some loader -> print_infos loader;
-    let loader = GI.Loader.set_build_path loader "tools/" in
-    GI.Loader.parse loader ~const_parser ~function_parser ~skip ()
+    let loader = Loader.set_build_path loader "tools/" in
+    Loader.parse loader ~const_parser ~function_parser ~skip ()
