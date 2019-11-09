0bae2eb4bff56c5f309e11012d0694a83ffed8a7
cedlemo <cedlemo@gmx.com>
2019-11-09 19:35:15 +0100

    Stubs: remove stupid and useless include

--- lib/Stubs.ml
+++ lib/Stubs.ml
@@ -1,9 +1,5 @@
 module Base_info = Bindings.Base_info.Enums(Bindings_stubs_base_info)
-include Base_info
-
 module Function_info = Bindings.Function_info.Flags(Bindings_stubs_function_info)
-include Function_info
-
 module Types = Bindings.Types.Enums(Bindings_stubs_types)
 module VFunc_info = Bindings.VFunc_info.Flags(Bindings_stubs_vfunc_info)
 
