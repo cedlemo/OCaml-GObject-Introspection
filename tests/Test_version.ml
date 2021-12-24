8d270643af8ac4bc8160651a70e24b7c1345f5df
Antonin Décimo <antonin.decimo@gmail.com>
2021-12-24 12:47:53 +0100

    Remove stdio from tests

--- tests/Test_version.ml
+++ tests/Test_version.ml
@@ -1,7 +1,14 @@
 open Test_utils
 open OUnit2
 open GObject_introspection
-open Stdio
+
+let input_lines ch =
+  let rec aux acc =
+    match input_line ch with
+    | exception End_of_file -> List.rev acc
+    | line -> aux (line :: acc)
+  in
+  aux []
 
 let test_get_version _ =
   let major = Version.get_major_version () |> string_of_int in
@@ -10,8 +17,9 @@ let test_get_version _ =
   let version = String.concat "." [ major; minor; micro ] in
   let run cmd =
     let inp = Unix.open_process_in cmd in
-    let r = In_channel.input_lines inp in
-    In_channel.close inp;
+    let r =
+      Fun.protect (fun () -> input_lines inp) ~finally:(fun () -> close_in inp)
+    in
     List.hd r
   in
   let modversion =
